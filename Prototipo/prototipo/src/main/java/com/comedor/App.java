da189752a8d4915d0be07636d641bb196db9b387
GodotCoffee28 <aaroncordobarivas@gmail.com>
2025-12-10 19:37:40 -0400

    Agregacion de la organizacion de las entregas y borrar codigo basura

--- Prototipo/prototipo/src/main/java/com/comedor/App.java
+++
@@ -1,8 +0,0 @@
-package com.comedor;
-
-public class App {
-    public static void main(String[] args) {
-        // Simplemente crea y muestra la ventana de Login
-        new Login().setVisible(true);
-    }
-}
