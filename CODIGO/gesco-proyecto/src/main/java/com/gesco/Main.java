86d98d9259f3d1aa833dc9ad41619d7b37844e5a
GodotCoffee28 <aaroncordobarivas@gmail.com>
2026-02-03 11:46:11 -0400

    Para quevean las carpetas

--- CODIGO/gesco-proyecto/src/main/java/com/gesco/Main.java
+++ CODIGO/gesco-proyecto/src/main/java/com/gesco/Main.java
@@ -4,8 +4,7 @@ package com.gesco;
  * Hello world!
  *
  */
-public class App 
-{
+public class Main{
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
---
+++ CODIGO/gesco-proyecto/src/main/java/com/gesco/views/RegistroView.java
@@ -0,0 +1,17 @@
+package com.gesco.views;
+
+import javax.swing.JFrame;
+
+
+
+public class RegistroView extends JFrame{
+
+    JFrame ventana = new JFrame("GESCO");
+    JLabel etiquetaSuperior = new JLabel();
+    JLabel etiquetaInferior = new JLabel();
+
+ 
+    public RegistroView() {
+            //El constructor de la clase
+        }
+}
