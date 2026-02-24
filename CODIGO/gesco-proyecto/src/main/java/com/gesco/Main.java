0617ba0a34daa05ba7c66545a9ddb8fa58664ddc
Alejandro Rondón <106562920+Alexuor@users.noreply.github.com>
2026-02-24 16:23:28 -0400

    ar

--- CODIGO/gesco-proyecto/src/main/java/com/gesco/Main.java
+++ CODIGO/gesco-proyecto/src/main/java/com/gesco/Main.java
@@ -1,12 +1,13 @@
 package com.gesco;
 
 import com.gesco.controllers.LogicaInterfaz;
+//import com.gesco.views.VistaMenuSemana;
+//import com.gesco.views.VistaMenuSemana;
 
 public class Main {
 
     public static void main(String[] args) {
         LogicaInterfaz logica = new LogicaInterfaz();
         logica.iniciar();
-
     }
 } 
