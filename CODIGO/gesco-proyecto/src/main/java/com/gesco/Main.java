cc47d1da5034333b1dcf3efc80f958267e647467
GodotCoffee28 <aaroncordobarivas@gmail.com>
2026-03-11 00:31:33 -0400

    Haciendo prueba del codigo Y dejando todo listo para el viernes

--- CODIGO/gesco-proyecto/src/main/java/com/gesco/Main.java
+++ CODIGO/gesco-proyecto/src/main/java/com/gesco/Main.java
@@ -7,8 +7,7 @@ public class Main {
     public static void main(String[] args) {
         LogicaInterfaz logica = new LogicaInterfaz();
         logica.iniciar();
-        //VistaCambiarTipoEstudiante  vista = new VistaCambiarTipoEstudiante();
-        //vista.setVisible(true);
     }
 }
 
+ 
